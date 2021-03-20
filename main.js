0bf31b739c894eb6c5e28f5832b8674b8fdf02de
SANTIAGORF24 <57204688+SANTIAGORF24@users.noreply.github.com>
2021-03-20 11:30:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,3 @@ ScrollReveal().reveal('.news-cards', { delay: 50});
 ScrollReveal().reveal('.cards-baner-two', { delay: 50});
 ScrollReveal().reveal('.cards-baner-one', { delay: 50});
 ScrollReveal().reveal('.social', { delay: 50});
-
-let tl = gsap.timeline({ repeat: 1, yoyo: true});
-tl.from('#hell', {
-    duration: 2,
-    rotate 360
-});
